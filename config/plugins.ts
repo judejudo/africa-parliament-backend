fd72508335c591f4de5aeb43a14de617accc2ec5
judejudo <ocomilj@gmail.com>
2025-11-29 07:32:15 +0300

    feature: add users-permissions configuration for content API

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default () => ({
+  'users-permissions': {
+    config: {
+      routes: {
+        'content-api': {
+          prefix: '',
+        },
+      },
+    },
+  },
+});
