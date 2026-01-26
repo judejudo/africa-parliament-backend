6d72a56dbba4c42b0cf5598f71187df582da15ff
judejudo <ocomilj@gmail.com>
2026-01-26 10:35:45 +0300

    commmit render danger

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,27 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://admin.an-pbo.org'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://admin.an-pbo.org'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'https://www.an-pbo.org', 'https://an-pbo.org'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
